5799464dfa539ee49451c1d459c2bc6b6574344f
CassioEngbras <cassio.murakami@engbras.com.br>
2024-07-22 08:10:52 -0300

    Final Changes

--- Cantilever_Beam/data/data_ex2.py
+++ Cantilever_Beam/data/data_ex2.py
@@ -11,14 +11,14 @@ import math
 
 # Domain definition:
 x1 = 0
-x2 = 1
+x2 = 2
 h = 0.1
 
 # Problem definition:
 L = x2 - x1
-E = 90
-I = 2
-P = 10
+E = 2*np.power(10, 6)
+I = 1.666/np.power(10, 5)
+P = 2000
 
 # Plot options:
 plot_solution = 'y'
@@ -65,7 +65,7 @@ def u_analytical(x):
     return -P/(6*E*I)*np.power(x, 3) + P*L/(2*E*I)*np.power(x, 2)
 
 def grad_u_analytical(x):
-    return -P/(2*E*I)*np.power(x, 2) + P*L/(E*I)*np.power(x, 1)
+    return -P/(2*E*I)*x*x + P*L/(E*I)*x
 
 def M_analytical(x):
     return P*(x - L)
